fa88ac410c54cbe7a3580bc7d62f218aa1a21fca
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-11-21 23:22:03 +0300

    the users application is configured

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email=os.getenv('EMAIL'),
+            first_name=os.getenv('FIRST_NAME'),
+            last_name=os.getenv('LAST_NAME'),
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+        )
+        user.set_password(os.getenv('PASSWORD'))
+        user.save()
